b68337e2c6daf5ae7410f7c4b285d29fbef3a0d2
Anastasia  Pankina <sia@Anastasias-MacBook-Air.local>
2019-08-10 08:51:26 +0100

    cat is rotating now

---
+++ rotating_cat/js/code.js
@@ -0,0 +1,9 @@
+
+function toggleClass() {
+  let image = document.getElementById("kitten");
+  if (image.className == 'image') {
+    image.className = '';
+  } else {
+    image.className = 'image';
+  }
+}
